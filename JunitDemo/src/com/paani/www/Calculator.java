160c6c5e469efcf69bf32f25b51defd51439f0ef
Training <Training@192.168.252.212>
2020-03-11 15:40:35 +0530

    new main method added

--- JunitDemo/src/com/paani/www/Calculator.java
+++ JunitDemo/src/com/paani/www/Calculator.java
@@ -1,5 +1,6 @@
 package com.paani.www;
 import java.lang.*;
+
 public class Calculator {
 	public double  add (double a , double b){
 		return a+b;
@@ -15,5 +16,12 @@ public class Calculator {
 			throw new ArithmeticException();
 		}
 		return a/b;
+	}
+	
+	public void main(String[] args) {
+		add(10,20);
+		subtract(20,10);
+		multiply(2,3);
+		divide(10,5);
 	}
 }
